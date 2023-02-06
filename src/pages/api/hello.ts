1fb8d85bdbfc529afc20a172205b4bb02f56f32b
Afaq Karim <ashfaqhunzai@gmail.com>
2023-02-06 21:09:59 +0500

    changes

--- src/pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
-
-import type { NextRequest } from "next/server";
-
-export const config = { runtime: "edge" };
-
-export default async function (req: NextRequest) {
-  return Response.json({ name: "John Doe" });
-}
